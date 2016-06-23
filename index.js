b45cc574e15d3c1d3c0e3bf11a2c4da2c546f179
Olivier Kaisin <olivier@coteclair.be>
2016-06-23 16:39:31 +0200

    Added limit, offset and proper Date conversion

---
+++ index.js
@@ -0,0 +1,33 @@
+'use strict';
+
+const sql = require('sql-bricks');
+
+const Select = sql.select;
+
+sql.conversions.Date = function (value) {
+  return value.toISOString();
+};
+
+Select.prototype.limit = function (val) {
+  this._limit = val;
+  return this;
+};
+
+Select.prototype.offset = function (val) {
+  this._offset = val;
+  return this;
+};
+
+Select.defineClause(
+  'limit',
+  '{{#ifNotNull _limit}}LIMIT {{_limit}}{{/ifNotNull}}',
+  { after: 'orderBy' }
+);
+
+Select.defineClause(
+  'offset',
+  '{{#ifNotNull _offset}}OFFSET {{_offset}}{{/ifNotNull}}',
+  { after: 'limit' }
+);
+
+module.exports = sql;
