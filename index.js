47e77953d8634b6c5bb32d6769dee9ba2ffadb3a
Olivier Kaisin <olivier@coteclair.be>
2016-06-24 15:46:42 +0200

    Fixed error introduced in 0.0.4

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 'use strict';
 
-const sql = require('sql-bricks');
+const _ = require('lodash');
+const sql = _.clone( require('sql-bricks') ); // No-conflict
 const reservedWords = require('./reserved-words');
 
 const Select = sql.select;
 
+// Support proper Date conversion
 sql.conversions.Date = function (value) {
   return '"' + value.toISOString() + '"';
 };
@@ -31,8 +33,11 @@ Select.defineClause(
   { after: 'limit' }
 );
 
+// Add support for reserved words escaping
 sql._autoQuoteChar = '`';
-sql._reserved = sql._reserved.concat(reservedWords);
 
+for (let word of reservedWords) {
+  sql._reserved[word] = word;
+}
 
 module.exports = sql;
